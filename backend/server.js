2943646a24b29f0e15344b9e02d92df198365465
Athira Suresh <us420334@gmail.com>
2025-11-12 10:19:06 +0530

    switch backend to cloud MongoDB

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connection string for MongoDB (we’ll add real one later)
-const mongoUri = process.env.MONGO_URL || 'mongodb://mongo:27017/dockerlearn';
-
-// Connect to MongoDB
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch(err => console.error("❌ MongoDB error:", err));
 
@@ -23,5 +20,4 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+app.listen(5000, () => console.log(" Backend running on port 5000"))
